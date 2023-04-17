61374aaadb4a9d87aa0a1795e185e213ebbf6650
0101oak <115372729+0101oak@users.noreply.github.com>
2023-04-17 12:46:59 +0300

    feat: dream team done

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  const dreamTeam = members
+        .filter(letters => typeof letters === 'string')
+        .map(letters => letters.toUpperCase().trim().charAt(0))
+        .sort()
+        .join('');
+
+  return dreamTeam;  
 }
 
 module.exports = {
